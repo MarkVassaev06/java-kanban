dc2640390ef39fc8438b2df02a417af60d6e98ef
yokulele96 <yokulele96@gmail.com>
2024-02-27 08:53:07 +0300

    Добавлены понятия эпика и подзадача.

---
+++ src/ru/mark/sprint4/Epic.java
@@ -0,0 +1,11 @@
+package ru.mark.sprint4;
+
+/**
+ * Эпика, включает в себя множество задач {@link Subtask}.
+ */
+public class Epic extends Task {
+
+    public Epic(int id, String name, String description) {
+        super(id, name, description);
+    }
+}
---
+++ src/ru/mark/sprint4/Subtask.java
@@ -0,0 +1,11 @@
+package ru.mark.sprint4;
+
+/**
+ * Подзадача, входит в эпику {@link Epic}.
+ */
+public class Subtask extends Task {
+
+    public Subtask(int id, String name, String description) {
+        super(id, name, description);
+    }
+}
