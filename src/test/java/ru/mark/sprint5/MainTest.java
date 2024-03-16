152877aed16b0a9924626428fe10d36acbab99f2
yokulele96 <yokulele96@gmail.com>
2024-03-16 17:52:16 +0300

    Добавляем тесты

---
+++ src/test/java/ru/mark/sprint5/MainTest.java
@@ -0,0 +1,134 @@
+package ru.mark.sprint5;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.mark.sprint5.manager.HistoryManager;
+import ru.mark.sprint5.manager.InMemoryTaskManager;
+import ru.mark.sprint5.manager.Managers;
+import ru.mark.sprint5.manager.TaskManager;
+import ru.mark.sprint5.models.Epic;
+import ru.mark.sprint5.models.Status;
+import ru.mark.sprint5.models.Subtask;
+import ru.mark.sprint5.models.Task;
+
+import java.util.List;
+
+public class MainTest {
+
+    private static TaskManager taskManager;
+    private static Task task1;
+    private static Task task2;
+
+    private static Epic epic1;
+    private static Subtask subtask11;
+    private static Subtask subtask12;
+    private static Epic epic2;
+    private static Subtask subtask21;
+
+    @BeforeAll
+    static void fillTasks() {
+        taskManager = new InMemoryTaskManager();
+        //Создайте две задачи,
+        task1 = new Task(taskManager.nextTaskId(), "task1", "description1");
+        taskManager.addTask(task1);
+        task2 = new Task(taskManager.nextTaskId(), "task2", "description2");
+        taskManager.addTask(task2);
+
+        //а также эпик с двумя подзадачами и эпик с одной подзадачей.
+        epic1 = new Epic(taskManager.nextTaskId(), "epic1", "epicDescription1");
+        taskManager.addEpic(epic1);
+        subtask11 = new Subtask(taskManager.nextTaskId(), "subtask11", "subtaskDescription11", epic1.getId());
+        taskManager.addSubtask(subtask11);
+        subtask12 = new Subtask(taskManager.nextTaskId(), "subtask12", "subtaskDescription12", epic1.getId());
+        taskManager.addSubtask(subtask12);
+
+        epic2 = new Epic(taskManager.nextTaskId(), "epic2", "epicDescription2");
+        taskManager.addEpic(epic2);
+        subtask21 = new Subtask(taskManager.nextTaskId(), "subtask21", "subtaskDescription21", epic2.getId());
+        taskManager.addSubtask(subtask21);
+    }
+
+    @Test
+    @DisplayName("Старые тесты")
+    void test1() {
+
+        List<Task> tasks = taskManager.getTasks();
+        Assertions.assertNotNull(tasks);
+        Assertions.assertEquals(2, tasks.size());
+
+        task1.setStatus(Status.DONE);
+        task2.setStatus(Status.IN_PROGRESS);
+
+        subtask12.setStatus(Status.DONE);
+        subtask11.setStatus(Status.DONE);
+        Assertions.assertEquals(Status.DONE, epic1.getStatus());
+
+        subtask21.setStatus(Status.IN_PROGRESS);
+        Assertions.assertEquals(Status.IN_PROGRESS, epic2.getStatus());
+
+        taskManager.removeSubtaskById(subtask11.getId());
+        List<Subtask> subtasks = taskManager.getAllSubtask();
+        Assertions.assertEquals(2, subtasks.size());
+    }
+
+    @Test
+    @DisplayName("Проверки равенства")
+    void equalsTest() {
+        Assertions.assertFalse(task1.equals(task2));
+        Assertions.assertFalse(epic1.equals(epic2));
+        Assertions.assertFalse(subtask11.equals(subtask12));
+        Assertions.assertFalse(subtask11.equals(subtask21));
+
+        Task t1 = new Task(1, "t1", "d1");
+        Task t2 = new Task(1, "t2", "d2");
+        Assertions.assertEquals(t1, t2);
+
+        Epic e1 = new Epic(2, "e1", "d1");
+        Epic e2 = new Epic(2, "e2", "d2");
+        Assertions.assertEquals(e1, e2);
+
+        Subtask s1 = new Subtask(1, "s1", "d1", 1);
+        Subtask s2 = new Subtask(1, "s2", "d2", 2);
+        Assertions.assertEquals(s1, s2);
+    }
+
+    @Test
+    @DisplayName("Epic нельзя добавить в самого себя. Subtask нельзя сделать своим же эпиком")
+    void epicTest() {
+        //В эпик добавить эпик невозможно, поскольку у меня используется жесткая типизация.
+        //Subtask нельзя сделать своим же эпиком по той же причине.
+    }
+
+    @Test
+    void managersTest() {
+
+        TaskManager manager = Managers.getDefault();
+        Assertions.assertNotNull(manager);
+
+        manager.addTask(task1);
+        Task taskById = manager.getTaskById(task1.getId());
+        Assertions.assertEquals(task1, taskById);
+
+        Assertions.assertEquals(task1.getName(), taskById.getName());
+        Assertions.assertEquals(task1.getDescription(), taskById.getDescription());
+        Assertions.assertEquals(task1.getStatus(), taskById.getStatus());
+
+        HistoryManager historyManager = Managers.getDefaultHistory();
+        Assertions.assertNotNull(historyManager);
+
+        historyManager.add(task1);
+        Task task = new Task(task1.getId(), task1.getName() + "new", task1.getDescription() + "new");
+        historyManager.add(task);
+        List<Task> history = historyManager.getHistory();
+        Assertions.assertEquals(2, history);
+        Assertions.assertEquals(history.get(0), history.get(1));
+        Assertions.assertTrue(
+                history.get(0).getName().endsWith("new") ||
+                        history.get(1).getName().endsWith("new")
+        );
+
+    }
+
+}
