c947cf84e3ca2368a92ece9a3140d802f6c8536f
yokulele96 <yokulele96@gmail.com>
2024-04-01 17:17:15 +0300

    заготовки

--- src/main/java/ru/mark/sprint5/manager/InMemoryHistoryManager.java
+++ src/main/java/ru/mark/sprint5/manager/InMemoryHistoryManager.java
@@ -1,27 +1,34 @@
 package ru.mark.sprint5.manager;
 
+
 import ru.mark.sprint5.models.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private static final int HISTORY_LIMIT = 10;
 
-    private List<Task> history;
+    private LinkedHashMap<Task> history;
 
     public InMemoryHistoryManager() {
-        history = new ArrayList<>();
+        history = new LinkedHashMap<>();
     }
 
     @Override
     public void add(Task task) {
         if (task != null) {
-            history.add(0, task);
+            history.linkLast(task);
             if (history.size() > HISTORY_LIMIT) {
                 history.remove(HISTORY_LIMIT);
             }
         }
+    }
+
+    private void removeNode(Node node) {
+        history.
     }
 
     @Override
@@ -34,7 +41,56 @@ public class InMemoryHistoryManager implements HistoryManager {
      */
     @Override
     public List<Task> getHistory() {
-        return List.copyOf(history);
+        return List.copyOf(history.getTasks());
     }
 
+    public static class LinkedHashMap<T extends Task> {
+        int size = 0;
+
+        Node<T> first;
+        Node<T> last;
+
+        Map<Integer, T> tasks = new HashMap<>();
+
+        public int size() {
+            return size;
+        }
+
+        void linkLast(T task) {
+            Node l = last;
+            Node newNode = new Node(task, l, null);
+            last = newNode;
+            if (l == null) {
+                first = newNode;
+            } else {
+                l.next = newNode;
+            }
+            size++;
+            tasks.put(task.getId(), task);
+        }
+
+        List<T> getTasks() {
+            List<T> result = new ArrayList<>();
+            Node<T> temp = first;
+            while (temp != null) {
+                result.add(temp.data);
+                temp = temp.next;
+            }
+            return result;
+        }
+
+    }
+
+    public static class Node<T> {
+
+        private Node prev;
+        private Node next;
+        private T data;
+
+        public Node(T data, Node prev, Node next) {
+            this.prev = prev;
+            this.next = next;
+            this.data = data;
+        }
+    }
 }
